1d2310d2ebc02a5418f536b6ffb1d86ce793cf2f
dkatsal <katsal.dimon@gmail.com>
2022-02-08 22:23:16 +0200

    fix customerstore

--- frontend/src/containers/MailBlank/MailBlank.tsx
+++ frontend/src/containers/MailBlank/MailBlank.tsx
@@ -5,16 +5,29 @@ import { useState } from 'react';
 import styles from './mailBlank.module.scss';
 import UserItem from 'components/Users/UserItem';
 import { mockdata } from 'components/Users/mockdata';
-import { useMutation } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { useForm } from 'react-hook-form';
 import { useStore } from 'hooks/useRootStore';
+import { ICustomerModel } from 'domain/customer';
 
 const MailBlank: FC = observer(() => {
-  const { customerStore } = useStore();
+  const { customerStore, preloadStore } = useStore();
+
   const history = useHistory();
   const { register, handleSubmit } = useForm();
-
+  console.log('>>>', customerStore);
   type IMessage = { to: string; from: string; subject: string; text: string };
+
+  const customerReq = useQuery<ICustomerModel[]>('customerReq', () => customerStore.getProfile(), {
+    onSuccess: () => {
+      preloadStore.setClose();
+    },
+    onError: () => {
+      preloadStore.setClose();
+      history.push('*');
+    },
+  });
+
   const sendMessage = useMutation((data: IMessage) => customerStore.sendMessage(data), {
     onSuccess: () => {
       history.push('/dashboard');
@@ -47,57 +60,61 @@ const MailBlank: FC = observer(() => {
   });
 
   return (
-    <main className={styles.formContainer}>
-      <Link className={styles.goBack} to='/dashboard'>
-        &#8592;
-      </Link>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <label className={styles.boxWide}>
-          <span className={styles.label}>To:</span>
-          <input
-            className={styles.input}
-            type='text'
-            {...register('to', { required: true })}
-            value={value}
-            onChange={(event) => {
-              setValue(event.target.value);
-            }}
-            onClick={inputClickHandler}
-            autoComplete={'off'}
-          />
-          {isOpen ? (
-            <ul className={styles.autocompleteMenu}>
-              {filteredMails.map((item, index) => (
-                <UserItem key={index} name={item.name} mail={item.mail} handleClick={handleClick} />
-              ))}
-            </ul>
-          ) : null}
-        </label>
-        <label className={styles.boxWide}>
-          <span className={styles.label}>From:</span>
-          <input
-            className={styles.input}
-            type='text'
-            value={customerStore.customer?.email ? `${customerStore.customer.email}` : `Anonim`}
-            {...register('from')}
-            readOnly
-          />
-        </label>
-        <label className={styles.boxWide}>
-          <span className={styles.label}>Topic:</span>
-          <input className={styles.input} type='text' {...register('subject')} autoComplete={'off'} />
-        </label>
-        <textarea className={styles.boxMessage} {...register('text')} placeholder='Say Hello...'>
-          {/* <span className={styles.label}>From</span> */}
-          {/* <input className={styles.input} type='text' name='message' placeholder='Say Hello...' /> */}
-        </textarea>
-        <div className={styles.boxBtn}>
-          <button className={styles.submitBtn} type='submit'>
-            Send
-          </button>
-        </div>
-      </form>
-    </main>
+    <>
+      {customerReq.isSuccess ? (
+        <main className={styles.formContainer}>
+          <Link className={styles.goBack} to='/dashboard'>
+            &#8592;
+          </Link>
+          <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <label className={styles.boxWide}>
+              <span className={styles.label}>To:</span>
+              <input
+                className={styles.input}
+                type='text'
+                {...register('to', { required: true })}
+                value={value}
+                onChange={(event) => {
+                  setValue(event.target.value);
+                }}
+                onClick={inputClickHandler}
+                autoComplete={'off'}
+              />
+              {isOpen ? (
+                <ul className={styles.autocompleteMenu}>
+                  {filteredMails.map((item, index) => (
+                    <UserItem key={index} name={item.name} mail={item.mail} handleClick={handleClick} />
+                  ))}
+                </ul>
+              ) : null}
+            </label>
+            <label className={styles.boxWide}>
+              <span className={styles.label}>From:</span>
+              <input
+                className={styles.input}
+                type='text'
+                value={`${customerStore.customer?.email}`}
+                {...register('from')}
+                readOnly
+              />
+            </label>
+            <label className={styles.boxWide}>
+              <span className={styles.label}>Topic:</span>
+              <input className={styles.input} type='text' {...register('subject')} autoComplete={'off'} />
+            </label>
+            <textarea className={styles.boxMessage} {...register('text')} placeholder='Say Hello...'>
+              {/* <span className={styles.label}>From</span> */}
+              {/* <input className={styles.input} type='text' name='message' placeholder='Say Hello...' /> */}
+            </textarea>
+            <div className={styles.boxBtn}>
+              <button className={styles.submitBtn} type='submit'>
+                Send
+              </button>
+            </div>
+          </form>
+        </main>
+      ) : null}
+    </>
   );
 });
 
